b486891aaa5041757c4df652c7638290d28cec7b
Brian Rios <bjr366@nyu.edu>
2021-12-15 15:33:44 -0500

    assgn 6: part 1

---
+++ assignment6/madlibs.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+    The <?php echo $_POST["adj1"]; ?> bear entered the <?php echo $_POST["ancient"]; ?> <?php echo $_POST["noun1"]; ?>.
+    There she <?php echo $_POST["verb1"]; ?> a <?php echo $_POST["noun2"]; ?> <?php echo $_POST["verb2"]; ?> with <?php echo $_POST["num"]; ?> <?php echo $_POST["noun3"]; ?>.s
+</body>
+</html>
